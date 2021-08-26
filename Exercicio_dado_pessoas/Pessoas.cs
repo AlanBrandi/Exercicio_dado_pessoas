ec43d3cd9a6155cef47b418b0a73eb268334c66f
Alan Brandi <alan.brandi@hotmail.com>
2021-08-26 12:06:29 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_dado_pessoas/Pessoas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_dado_pessoas
+{
+    class Pessoas
+    {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ Exercicio_dado_pessoas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio_dado_pessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoas x, y;
+
+            x = new Pessoas();
+            y = new Pessoas();
+
+            Console.WriteLine("Digite os dados da primeira pessoa(Nome e idade, respectivamente):");
+            x.nome = Console.ReadLine();
+            x.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os dados da segunda pessoa(Nome e idade, respectivamente):");
+            y.nome = Console.ReadLine();
+            y.idade = int.Parse(Console.ReadLine());
+
+            if (x.idade > y.idade)
+            {
+                Console.WriteLine("A pessoa mais velha é: " + x.nome);
+            }
+            else
+            {
+                Console.WriteLine("A pessoa mais velha é: " + y.nome);
+            }
+        }
+    }
+}
